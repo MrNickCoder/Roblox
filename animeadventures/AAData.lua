94fd32da2bfa79aaca52fa4bc72bab517240fe58
NickCoder <rexgorre@gmail.com>
2023-07-25 23:37:57 +0800

    Create AAData.lua

---
+++ animeadventures/AAData.lua
@@ -0,0 +1,222 @@
+return {
+	["World Type"] = {
+		["Types"] = {
+			"Story Worlds","Legend Stages","Raid Worlds","Infinity Castle","Portals","Dungeon","Secret Portals"
+		};
+		["Type"] = {
+			["Story Worlds"] = {
+				["Worlds"] = {
+					"Planet Namak","Shiganshinu District","Snowy Town","Hidden Sand Village","Marine's Ford","Ghoul City","Hollow World","Ant Kingdom",
+					"Magic Town","Cursed Academy","Clover Kingdom","Cape Canaveral","Alien Spaceship","Fabled Kingdom","Hero City","Puppet Island",
+					"Virtual Dungeon","Windhym"
+				};
+				["World"] = {
+					["Planet Namak"] = {
+						["Levels"] = {"namek_infinite","namek_level_1","namek_level_2","namek_level_3","namek_level_4","namek_level_5","namek_level_6"};
+					};
+					["Shiganshinu District"] = {
+						["Levels"] = {"aot_infinite","aot_level_1","aot_level_2","aot_level_3","aot_level_4","aot_level_5","aot_level_6"};
+					};
+					["Snowy Town"] = {
+						["Levels"] = {"demonslayer_infinite","demonslayer_level_1","demonslayer_level_2","demonslayer_level_3","demonslayer_level_4","demonslayer_level_5","demonslayer_level_6"};
+					};
+					["Hidden Sand Village"] = {
+						["Levels"] = {"naruto_infinite","naruto_level_1","naruto_level_2","naruto_level_3","naruto_level_4","naruto_level_5","naruto_level_6"};
+					};
+					["Marine's Ford"] = {
+						["Levels"] = {"marineford_infinite","marineford_level_1","marineford_level_2","marineford_level_3","marineford_level_4","marineford_level_5","marineford_level_6"};
+					};
+					["Ghoul City"] = {
+						["Levels"] = {"tokyoghoul_infinite","tokyoghoul_level_1","tokyoghoul_level_2","tokyoghoul_level_3","tokyoghoul_level_4","tokyoghoul_level_5","tokyoghoul_level_6"};
+					};
+					["Hollow World"] = {
+						["Levels"] = {"hueco_infinite","hueco_level_1","hueco_level_2","hueco_level_3","hueco_level_4","hueco_level_5","hueco_level_6"};
+					};
+					["Ant Kingdom"] = {
+						["Levels"] = {"hxhant_infinite","hxhant_level_1","hxhant_level_2","hxhant_level_3","hxhant_level_4","hxhant_level_5","hxhant_level_6"};
+					};
+					["Magic Town"] = {
+						["Levels"] = {"magnolia_infinite","magnolia_level_1","magnolia_level_2","magnolia_level_3","magnolia_level_4","magnolia_level_5","magnolia_level_6"};
+					};
+					["Cursed Academy"] = {
+						["Levels"] = {"jjk_infinite","jjk_level_1","jjk_level_2","jjk_level_3","jjk_level_4","jjk_level_5","jjk_level_6"};
+					};
+					["Clover Kingdom"] = {
+						["Levels"] = {"clover_infinite","clover_level_1","clover_level_2","clover_level_3","clover_level_4","clover_level_5","clover_level_6"};
+					};
+					["Cape Canaveral"] = {
+						["Levels"] = {"jojo_infinite","jojo_level_1","jojo_level_2","jojo_level_3","jojo_level_4","jojo_level_5","jojo_level_6"};
+					};
+					["Alien Spaceship"] = {
+						["Levels"] = {"opm_infinite","opm_level_1","opm_level_2","opm_level_3","opm_level_4","opm_level_5","opm_level_6"};
+					};
+					["Fabled Kingdom"] = {
+						["Levels"] = {"7ds_infinite","7ds_level_1","7ds_level_2","7ds_level_3","7ds_level_4","7ds_level_5","7ds_level_6"};
+					};
+					["Hero City"] = {
+						["Levels"] = {"mha_infinite","mha_level_1","mha_level_2","mha_level_3","mha_level_4","mha_level_5","mha_level_6"};
+					};
+					["Puppet Island"] = {
+						["Levels"] = {"dressrosa_infinite","dressrosa_level_1","dressrosa_level_2","dressrosa_level_3","dressrosa_level_4","dressrosa_level_5","dressrosa_level_6"};
+					};
+					["Virtual Dungeon"] = {
+						["Levels"] = {"sao_infinite","sao_level_1","sao_level_2","sao_level_3","sao_level_4","sao_level_5","sao_level_6"};
+					};
+					["Windhym"] = {
+						["Levels"] = {"berserk_infinite","berserk_level_1","berserk_level_2","berserk_level_3","berserk_level_4","berserk_level_5","berserk_level_6"};
+					};
+				};
+			};
+			["Legend Stages"] = {
+				["Worlds"] = {
+					"Clover Kingdom (Elf Invasion)","Hollow Invasion","Cape Canaveral (Legend)","Fabled Kingdom (Legend)",
+					"Hero City (Midnight)","Virtual Dungeon (Bosses)"
+				};
+				["World"] = {
+					["Clover Kingdom (Elf Invasion)"] = {
+						["Levels"] = {"clover_legend_1","clover_legend_2","clover_legend_3"};
+					};
+					["Hollow Invasion"] = {
+						["Levels"] = {"bleach_legend_1","bleach_legend_2","bleach_legend_3","bleach_legend_4","bleach_legend_5","bleach_legend_6"};
+					};
+					["Cape Canaveral (Legend)"] = {
+						["Levels"] = {"jojo_legend_1","jojo_legend_2","jojo_legend_3"};
+					};
+					["Fabled Kingdom (Legend)"] = {
+						["Levels"] = {"7ds_legend_1","7ds_legend_2","7ds_legend_3"};
+					};
+					["Hero City (Midnight)"] = {
+						["Levels"] = {"mha_legend_1","mha_legend_2","mha_legend_3","mha_legend_4","mha_legend_5","mha_legend_6"};
+					};
+					["Virtual Dungeon (Bosses)"] = {
+						["Levels"] = {"sao_legend_1","sao_legend_2","sao_legend_3"};
+					};
+				};
+			},
+			["Raid Worlds"] = {
+				["Worlds"] = {
+					"Storm Hideout","West City","Infinity Train","Shiganshinu District - Raid","Hiddel Sand Village - Raid","Freezo's Invasion",
+					"Entertainment District", "Hero City (Hero Slayer)","Marine's Ford (Buddha)"
+				};
+				["World"] = {
+					["Storm Hideout"] = {
+						["Levels"] = {"uchiha_level_1","uchiha_level_2","uchiha_level_3","uchiha_level_4","uchiha_level_5"};
+					};
+					["West City"] = {
+						["Levels"] = {"west_city_raid"};
+					};
+					["Infinity Train"] = {
+						["Levels"] = {"demonslayer_raid_1"};
+					};
+					["Shiganshinu District - Raid"] = {
+						["Levels"] = {"aot_raid_1"};
+					};
+					["Hiddel Sand Village - Raid"] = {
+						["Levels"] = {"naruto_raid_1"};
+					};
+					["Freezo's Invasion"] = {
+						["Levels"] = {"west_city_frieza_level_1","west_city_frieza_level_2","west_city_frieza_level_3","west_city_frieza_level_4","west_city_frieza_level_5"};
+					};
+					["Entertainment District"] = {
+						["Levels"] = {"entertainment_district_level_1","entertainment_district_level_2","entertainment_district_level_3","entertainment_district_level_4","entertainment_district_level_5"};
+					};
+					["Hero City (Hero Slayer)"] = {
+						["Levels"] = {"mha_stain"};
+					};
+					["Marine's Ford (Buddha)"] = {
+						["Levels"] = {"marineford_sengoku"};
+					};
+				}
+			};
+			["Portals"] = {
+				["Worlds"] = {
+					"Alien Portals","Zeldris Portals","Demon Portals","Dressrosa Portals","Madoka Portals","The Eclipse"
+				};
+				["World"] = {
+					["Alien Portals"] = {
+						["Levels"] = {"portal_boros_g"};
+						["Level"] = {
+							["portal_boros_g"] = {
+								["Damage Modifier"] = {"Magic","Physical","Fire","Aqua","Storm","Air","Rose","Dark","Light"};
+							};
+						};
+					};
+					["Zeldris Portals"] = {
+						["Levels"] = {"april_portal_item"};
+						["Level"] = {
+							["april_portal_item"] = {
+								["Damage Modifier"] = {"Magic","Physical","Fire","Aqua","Storm","Air","Rose","Dark","Light"};
+							};
+						};
+					};
+					["Demon Portals"] = {
+						["Levels"] = {"portal_zeldris"};
+						["Level"] = {
+							["portal_zeldris"] = {
+								["Damage Modifier"] = {"Magic","Physical","Fire","Aqua","Storm","Air","Rose","Dark","Light"};
+							};
+						};
+					};
+					["Dressrosa Portals"] = {
+						["Levels"] = {"portal_item__dressrosa"};
+						["Level"] = {
+							["portal_item__dressrosa"] = {
+								["Damage Modifier"] = {"Magic","Physical","Fire","Aqua","Storm","Air","Rose","Dark","Light"};
+							};
+						};
+					};
+					["Madoka Portals"] = {
+						["Levels"] = {"portal_item__madoka"};
+						["Level"] = {
+							["portal_item__madoka"] = {
+								["Tiers"] = {"1","2","3","4","5","6","7","8","9","10","11"};
+								["Damage Modifier"] = {"Magic","Physical"};
+								["Challenge"] = {"double_cost","short_range","fast_enemies","regen_enemies","tank_enemies","shield_enemies"};
+							};
+						};
+					};
+					["The Eclipse"] = {
+						["Levels"] = {"portal_item__eclipse"};
+						["Level"] = {
+							["portal_item__eclipse"] = {
+								["Damage Modifier"] = {"Magic","Physical","Fire","Aqua","Storm","Air","Rose","Dark","Light"};
+							};
+						};
+					};
+				};
+			};
+			["Dungeon"] = {
+				["Worlds"] = {
+					"Cursed Womb","Crused Parade","Anniversary Island"
+				};
+				["World"] = {
+					["Cursed Womb"] = {
+						["Levels"] = {"jjk_finger"};
+					};
+					["Crused Parade"] = {
+						["Levels"] = {"jjk_raid"};
+					};
+					["Anniversary Island"] = {
+						["Levels"] = {"namek_anniversary"};
+					};
+				}
+			};
+			["Secret Portals"] = {
+				["Worlds"] = {
+					"Dressrosa Secret Portals","Madoka Secret Portals","The Eclipse Secret"
+				};
+				["World"] = {
+					["Dressrosa Secret Portals"] = {
+						["Levels"] = {"portal_item__doflamingo"};
+					};
+					["Madoka Secret Portals"] = {
+						["Levels"] = {"portal_item__madoka2"};
+					};
+					["The Eclipse Secret"] = {
+						["Levels"] = {"portal_item__femto"};
+					};
+				};
+			};
+		}
+	};
+}
