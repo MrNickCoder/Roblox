dba0b5f78160c48105ca2dc70134e69a5cc2ca81
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2025-03-02 22:33:47 +0800

    Core Scripts

---
+++ Core.lua
@@ -0,0 +1,11 @@
+local HttpService = game:GetService("HttpService")
+local Games = (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MrNickCoder/Roblox/refs/heads/main/Games.lua"))()
+
+local PlaceID = game.PlaceId
+local UniverseID = HttpService:JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/".. PlaceID .."/universe")).universeId
+
+if Games["UniverseIDs"][UniverseID] then
+	(loadstring or load)(game:HttpGet(Games["UniverseIDs"][UniverseID]))()
+elseif Games["PlaceIDS"][PlaceID] then
+	(loadstring or load)(game:HttpGet(Games["PlaceIDS"][PlaceID]))()
+end
---
+++ Games.lua
@@ -0,0 +1,8 @@
+return {
+	["UniverseIDs"] = {
+		[2239430935] = "https://raw.githubusercontent.com/MrNickCoder/Roblox/refs/heads/main/script/BLAIR.lua"; -- BLAIR
+	};
+	["PlaceIDs"] = {
+		
+	};
+}
