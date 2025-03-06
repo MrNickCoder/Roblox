320874865f2206197cb6f70e5b475f2eb1f2219a
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2025-03-07 03:29:24 +0800

    Update bLockerman's Minesweeper.lua

--- scripts/bLockerman's Minesweeper.lua
+++ scripts/bLockerman's Minesweeper.lua
@@ -11,7 +11,7 @@ local Closed = { Color3.fromRGB(103, 180, 88); Color3.fromRGB(117, 205, 100); }
 ---------------------
 function getBlockAtPosition(position)
 	for _, v in pairs(Tiles:GetChildren()) do
-		if (v.Position - position).Magnitude == 0 then return v; end
+		if (v.Position - position).Magnitude <= 1 then return v; end
 	end
 	return nil;
 end
@@ -19,7 +19,20 @@ function getNeighbors(block)
 	local blockPos = block.Position;
 	local blockSize = block.Size;
 	
-	return {
+	local offset = {
+		[-1, -1]; [0, -1]; [1, -1];
+		[-1,  0];          [1,  0];
+		[-1,  1]; [0,  1]; [1,  1];
+	};
+	
+	local neighbors = {}
+	for _, v in pairs(offset) do
+		local block = getBlockAtPosition(blockPos + Vector3.new(blockSize.X * v[0], 0, blockSize.Z * v[1]));
+		table.insert(neighbors, block);
+	end
+	
+	return neighbors
+	--[[return {
 		getBlockAtPosition(blockPos + Vector3.new(blockSize.X, 0, blockSize.Z));
 		getBlockAtPosition(blockPos + Vector3.new(blockSize.X, 0, 0));
 		getBlockAtPosition(blockPos + Vector3.new(blockSize.X, 0, -blockSize.Z));
@@ -28,7 +41,8 @@ function getNeighbors(block)
 		getBlockAtPosition(blockPos + Vector3.new(-blockSize.X, 0, 0));
 		getBlockAtPosition(blockPos + Vector3.new(-blockSize.X, 0, blockSize.Z));
 		getBlockAtPosition(blockPos + Vector3.new(0, 0, blockSize.Z));
-	}
+	}]]
 end
 function isSafe()
+	
 end
