31bbc86f139f980f2a77de718d00543e986a2ceb
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2025-03-09 01:42:18 +0800

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,6 +1,6 @@
 return {
 	["UniverseIDs"] = {
-		[2239430935] = "https://raw.githubusercontent.com/MrNickCoder/Roblox/refs/heads/main/games/Blair_Script.lua"; -- BLAIR
+		[2239430935] = "https://raw.githubusercontent.com/MrNickCoder/Roblox/refs/heads/main/games/Blair.lua"; -- BLAIR
 	};
 	["PlaceIDs"] = {
 		[0] = "";
