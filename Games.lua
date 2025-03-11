f720a0fbe099fe5f10dfc73c2589d0b14163ad51
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2025-03-11 22:19:44 +0800

    Added Dead Rails

--- Games.lua
+++ Games.lua
@@ -1,6 +1,7 @@
 return {
 	["UniverseIDs"] = {
 		[2239430935] = "https://raw.githubusercontent.com/MrNickCoder/Roblox/refs/heads/main/games/Blair.lua"; -- Blair
+		[7018190066] = "https://raw.githubusercontent.com/MrNickCoder/Roblox/refs/heads/main/games/Dead%20Rails.lua" -- Dead Rails
 	};
 	["PlaceIDs"] = {
 		[0] = "";
