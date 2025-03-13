55d74b274bf55992f4c458ba9a679212d84d7ad4
NickCoder <16845999+MrNickCoder@users.noreply.github.com>
2025-03-13 23:28:48 +0800

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -4,6 +4,6 @@ return {
 		[7018190066] = "https://raw.githubusercontent.com/MrNickCoder/Roblox/refs/heads/main/games/Dead%20Rails.lua" -- Dead Rails
 	};
 	["PlaceIDs"] = {
-		[0] = "";
+		[73010525850196] = "https://raw.githubusercontent.com/MrNickCoder/Roblox/refs/heads/main/games/Desert%20Detectors.lua"; -- Desert Detectors
 	};
 }
